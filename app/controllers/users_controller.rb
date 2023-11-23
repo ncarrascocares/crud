763a1afcaf666930c05d26f254e19babd481287b
ncarrascocares <nicolascarrasco15@gmail.com>
2023-11-23 16:53:21 -0300

    Creacion de guardado del proyecto crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
+    #El metodo create recibe las variables que se envian desde el formulario que se creo en el index.html.erb cuando se presiona el btn submit
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        redirect_to root_path, success: "User created successfully."
+      else
+        render :new, error: "Something went wrong."
+       end
+    end
+
+    private
+    def user_params
+        params.require(:user).permit(:fullname, :username, :age, :address, :email, :password_digest )
+    end
+
 end
